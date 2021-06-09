20f35785b0a8fada63dbaeb107005d63302dd19e
ozyr501 <ozyr501@gmail.com>
2021-06-09 15:34:04 +0200

    Zmiana menu
    Nowe, lepsze menu

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function showDropdown(){
     $("#dropdown").removeClass("hide").addClass("show");
-    $("#ddlogin").attr("src", "icon/cancel.png");
 }
 
 function hideDropdown(){
     $("#dropdown").removeClass("show").addClass("hide");
-    $("#ddlogin").attr("src", "icon/login.png");
 }
