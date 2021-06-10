6e67b6ee6592bb81502a45949a12bf42805738d0
ozyr501 <ozyr501@gmail.com>
2021-06-10 11:22:46 +0200

    Drobne poprawki

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function showDropdown(){
 function hideDropdown(){
     $("#dropdown").removeClass("show").addClass("hide");
 }
+
+function showMenu(){
+    $("#menu-bar").removeClass("MenuHide").addClass("MenuShow");
+    $("#menubutton img").attr('src', 'icon/cancel.png')
+}
+
+function hideMenu() {
+    $("#menu-bar").removeClass("MenuShow").addClass("MenuHide");
+    $("#menubutton img").attr("src", "icon/Hamburger.png");
+}
