87d841c998d25557e22592cdd0e24132f629aa46
ozyr501 <ozyr501@gmail.com>
2021-06-12 18:22:31 +0200

    Logowanie i rejestracja
    Dodanie funkcji logowania i rejestrowania się

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+    session_start();
+    require("funkcje.php");
+    /* Dane admina:
+        login: UserAdmin
+        pass: SilneHaslo123
+    */
+    $link = mysqli_connect("localhost", "UserAdmin", "SilneHaslo123", "kino");
+    if(!$link)
+    {
+        echo("Błąd bazy danych. Spróbuj ponownie później");
+        exit();
+    }
+    if (!isset($_POST['login']) || !isset($_POST['pass']))
+    {
+        echo("Błąd formularza");
+        exit();
+    }
+    $login = $_POST['login'];
+    $pass = $_POST['pass'];
+
+    $que = "SELECT LOGIN FROM users WHERE login = ? AND haslo = ?";
+    $pstmt = mysqli_stmt_init($link);
+    mysqli_stmt_prepare($pstmt, $que);
+    mysqli_stmt_bind_param($pstmt,"ss",$login,$pass);
+    mysqli_stmt_execute($pstmt);
+    mysqli_stmt_bind_result($pstmt, $logins);
+    mysqli_stmt_fetch($pstmt);
+    if($logins === $login)
+    {
+        $_SESSION['logged'] = true;
+        $_SESSION['login'] = $login;
+        header('Location: index.php');
+    }
+    else{
+        $_SESSION['logged'] = false;
+        $_SESSION['mess'] = 'Błędne dane';
+        header('Location: loginpage.php');
+    }
+    mysqli_stmt_close($pstmt);
+    mysqli_close($link);
+?>
